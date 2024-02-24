29446d6b5ce3009560ce4230bd5874cb31955e32
Raunak Mishra <raunakm725@gmail.com>
2024-02-24 23:44:14 +0530

    malfunctioned pop up menu created

--- lib/screens/admin_dashboard.dart
+++ lib/screens/admin_dashboard.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,120 +11,150 @@ class AdminDashBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Dashboard',
-                      style: GoogleFonts.poppins(
-                        fontSize: 36.0,
-                        fontWeight: FontWeight.bold,
-                        color: const Color.fromARGB(255, 2, 80, 145)
-                            .withOpacity(1.0),
-                        shadows: [
-                          Shadow(
-                            color: const Color.fromARGB(153, 0, 0, 0)
-                                .withOpacity(0.3),
-                            offset: const Offset(2, 2),
-                            blurRadius: 2,
-                          ),
-                        ],
-                      ),
-                    ),
-                    PopupMenuButton(
-                      offset: const Offset(
-                          2010, 70), //shifting the popmenubutton downwards
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          value: 'manage_profile',
-                          child: Text(
-                            'Manage Profile',
-                            style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontFamily: 'Roboto', //font changed
-                            ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Dashboard',
+                          style: GoogleFonts.poppins(
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 2, 80, 145)
+                                .withOpacity(1.0),
+                            shadows: [
+                              Shadow(
+                                color: const Color.fromARGB(153, 0, 0, 0)
+                                    .withOpacity(0.3),
+                                offset: const Offset(2, 2),
+                                blurRadius: 2,
+                              ),
+                            ],
                           ),
                         ),
-                        const PopupMenuItem(
-                          value: 'logout',
-                          child: Text(
-                            'Logout',
-                            style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontFamily: 'Roboto',
+                        PopupMenuButton(
+                          offset: const Offset(
+                              2010, 70), //shifting the popmenubutton downwards
+                          itemBuilder: (context) => [
+                            const PopupMenuItem(
+                              value: 'manage_profile',
+                              child: Text(
+                                'Manage Profile',
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontFamily: 'Roboto', //font changed
+                                ),
+                              ),
+                            ),
+                            const PopupMenuItem(
+                              value: 'logout',
+                              child: Text(
+                                'Logout',
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontFamily: 'Roboto',
+                                ),
+                              ),
+                            ),
+                          ],
+                          color: const Color.fromARGB(255, 225, 237, 248),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          onSelected: (value) {
+                            if (value == 'manage_profile') {
+                              // Handle Manage Profile action
+                            } else if (value == 'logout') {
+                              // Handle Logout action
+                            }
+                          },
+                          icon: Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.grey, // Change this color as needed
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/1.jpg'), // Replace this with your image path
+                                fit: BoxFit.cover,
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color.fromARGB(64, 0, 0, 0)
+                                      .withOpacity(0.4),
+                                  spreadRadius: 1,
+                                  blurRadius: 4,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
                             ),
                           ),
                         ),
                       ],
-                      color: const Color.fromARGB(255, 225, 237, 248),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    const SizedBox(
+                        height: 0.1), // Adding some space between the texts
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                      onSelected: (value) {
-                        if (value == 'manage_profile') {
-                          // Handle Manage Profile action
-                        } else if (value == 'logout') {
-                          // Handle Logout action
-                        }
-                      },
-                      icon: Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.grey, // Change this color as needed
-                          image: const DecorationImage(
-                            image: AssetImage(
-                                'assets/1.jpg'), // Replace this with your image path
-                            fit: BoxFit.cover,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: const Color.fromARGB(64, 0, 0, 0)
-                                  .withOpacity(0.4),
-                              spreadRadius: 1,
-                              blurRadius: 4,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
+                      child: const Text(
+                        'ADMIN',
+                        style: TextStyle(
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 3.0,
+                          color: Colors.black,
                         ),
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(
-                    height: 0.1), // Adding some space between the texts
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: const Text(
-                    'ADMIN',
-                    style: TextStyle(
-                      fontSize: 10.0,
-                      fontWeight: FontWeight.w900,
-                      letterSpacing: 3.0,
-                      color: Colors.black,
+              ),
+              Expanded(
+                child: Container(
+                    // Your main body content goes here
                     ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Container(
-                // Your main body content goes here
+          // Add this to blur the background when the bottom sheet is shown
+          Builder(
+            builder: (context) {
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                        child: Container(
+                          color: Colors.black.withOpacity(0),
+                        ),
+                      );
+                    },
+                    isScrollControlled: true,
+                    backgroundColor: Colors.transparent,
+                  );
+                },
+                child: Container(
+                  color: Colors.transparent,
                 ),
+              );
+            },
           ),
         ],
       ),
@@ -176,25 +207,61 @@ class AdminDashBoard extends StatelessWidget {
                   ),
                   Transform.translate(
                     offset: const Offset(0, -30),
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: const Color.fromARGB(255, 31, 122, 182),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(64, 0, 0, 0)
-                                .withOpacity(0.4),
-                            spreadRadius: 1,
-                            blurRadius: 4,
-                            offset: const Offset(0, 2),
-                          ),
-                        ],
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
+                    child: InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Wrap(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: const Icon(Icons.event),
+                                  title: const Text('EVENTS'),
+                                  onTap: () {
+                                    // Handle EVENTS action
+                                  },
+                                ),
+                                ListTile(
+                                  leading: const Icon(Icons.developer_board),
+                                  title: const Text('RESEARCH & DEVELOPMENT'),
+                                  onTap: () {
+                                    // Handle RESEARCH & DEVELOPMENT action
+                                  },
+                                ),
+                                ListTile(
+                                  leading: const Icon(Icons.manage_accounts),
+                                  title: const Text('ROLE MANAGEMENT'),
+                                  onTap: () {
+                                    // Handle ROLE MANAGEMENT action
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                          isScrollControlled: true,
+                          backgroundColor: Colors.transparent,
+                        );
+                      },
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color.fromARGB(255, 31, 122, 182),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color.fromARGB(64, 0, 0, 0)
+                                  .withOpacity(0.4),
+                              spreadRadius: 1,
+                              blurRadius: 4,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
